15aee49e0f79d8237662b5674d63cb16af09ef57
Karim Farhi <ka.farhi@gmail.com>
2022-08-16 13:08:57 +0200

    refactorisation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,209 +7,167 @@ function editNav() {
   }
 }
 
+const OpenModal = (selector) => { selector.style.display = "block" }
+const CloseModal = (selector) => { selector.style.display = "none" }
+
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const formClose = document.querySelectorAll(".close");
-const validationClose = document.getElementById("validation-close")
+const validationClose = document.getElementById("validation-close");
+const modalValidation = document.getElementById("validation");
+
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthDate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const checkLocation = document.getElementsByName("location");
+const checkbox = document.getElementById("checkbox1");
+
+const errorFirstName = firstName.nextElementSibling.nextElementSibling;
+const errorLastName = lastName.nextElementSibling.nextElementSibling;
+const errorEmail = email.nextElementSibling.nextElementSibling;
+const errorBirthDate = birthDate.nextElementSibling.nextElementSibling;
+const errorQuantity = quantity.nextElementSibling;
+const errorLocation = document.getElementById("error-msg-location");
+const errorCheckBox = checkbox.nextElementSibling.nextElementSibling.nextElementSibling;
 
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
+modalBtn.forEach((btn) => btn.addEventListener("click", () => OpenModal(modalbg)));
 
 // Close modal event
-formClose.forEach((btn) => btn.addEventListener("click", closeModal));
+formClose.forEach((btn) => btn.addEventListener("click", () => CloseModal(modalbg)));
 
-// Close modal function
-function closeModal() {
-  modalbg.style.display = "none";
-  console.log("test")
-}
+//Close modal
+modalValidation.onclick = (event) => { CloseModal(modalbg) }
 
 
 //validation de formulaire
 
-
-let formError = 0
-
-function formValidation(event) {
-
-
-  //Sélection DOM
-  let firstName = document.querySelector("#first");
-  let lastName = document.querySelector("#last");
-  let email = document.querySelector("#email");
-  let birthDate = document.querySelector("#birthdate");
-  let quantity = document.querySelector("#quantity");
-  let checkLocation = document.getElementsByName("location");
-  let checkbox = document.getElementById("checkbox1");
-
-
-  let errorFirstName = firstName.nextElementSibling.nextElementSibling;
-  let errorLastName = lastName.nextElementSibling.nextElementSibling;
-  let errorEmail = email.nextElementSibling.nextElementSibling;
-  let errorBirthDate = birthDate.nextElementSibling.nextElementSibling;
-  let errorQuantity = quantity.nextElementSibling;
-  let errorLocation = document.getElementById("error-msg-location")
-  let errorCheckBox = checkbox.nextElementSibling.nextElementSibling.nextElementSibling;
+function formValidation() {
+  const e1 = firstNameValidation(),
+    e2 = lastNameValidation(),
+    e3 = emailValidation(),
+    e4 = birthDateValidation(),
+    e5 = quantityValidation(),
+    e6 = cityValidation(),
+    e7 = CGUValidation();
+  return (e1 && e2 && e3 && e4 && e5 && e6 && e7);
+}
 
 
 
-  //Validation Prénom
-  if (firstName.value.length != 0) {
-    if (firstName.value.length < 2) {
-      errorFirstName.textContent = "Veuillez renseigner plus de 2 lettres";
-      formError = formError + 1
+//Validation Prénom
+function firstNameValidation() {
+  errorFirstName.textContent = "";
 
-    } else {
-      errorFirstName.textContent = "";
-
-    }
-  } else {
-    errorFirstName.textContent = "Veuillez remplir ce champ";
-    formError = formError + 1
-
+  if (firstName.value.length < 2) {
+    errorFirstName.textContent = "Veuillez renseigner plus de 2 lettres";
+    return false;
   }
+  return true;
+}
 
-  //Validation nom de famille
 
-  if (lastName.value.length != 0) {
-    if (lastName.value.length < 2) {
-      errorLastName.textContent = "Veuillez renseigner plus de 2 lettres";
-      formError = formError + 1
+//Validation nom de famille
+function lastNameValidation() {
+  errorLastName.textContent = "";
 
-    } else {
-      errorLastName.textContent = "";
-
-    }
-  } else {
-    errorLastName.textContent = "Veuillez remplir ce champ";
-    formError = formError + 1
-
+  if (lastName.value.length < 2) {
+    errorLastName.textContent = "Veuillez renseigner plus de 2 lettres";
+    return false;
   }
+  return true;
+}
 
 
-  //Validation Email
+//Validation Email
+function emailValidation() {
+  errorEmail.textContent = ""
   const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (email) {
-    let testEmail = regexEmail.test(email.value);
-    if (testEmail) {
-      errorEmail.textContent = ""
 
-    } else {
-      errorEmail.textContent = "Veuillez insérer un email valide"
-      formError = formError + 1
-
-    }
-
-  } else {
-    errorEmail.textContent = "Veuillez remplir ce champ"
-    formError = formError + 1
-
+  if (!regexEmail.test(email.value)) {
+    errorEmail.textContent = "Veuillez insérer un email valide"
+    return false;
   }
+  return true;
+}
 
 
-  //Validation Date de Naissance
+//Validation Date de Naissance
+
+function birthDateValidation(event) {
+  errorBirthDate.textContent = ""
   const regexBirthDate = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
 
-  let testBirthDate = regexBirthDate.test(birthDate.value);
-  if (birthDate) {
-
-
-    if (testBirthDate) {
-      errorBirthDate.textContent = ""
-
-    } else {
-      errorBirthDate.textContent = "Insérer date de naissance valide"
-      formError = formError + 1
-    }
-  } else {
-    errorBirthDate.textContent = "INSERER DATE DE NAISSANCE"
-    formError = formError + 1
-
-
-
+  if (!regexBirthDate.test(birthDate.value)) {
+    errorBirthDate.textContent = "Insérer date de naissance valide"
+    return false;
   }
-
-
-  //Validation nombre tournoi
-  const regexQuantity = /^(0|[1-9][0-9]*)$/;
-  if (quantity) {
-    let testQuantity = regexQuantity.test(quantity.value);
-    if (testQuantity) {
-      errorQuantity.textContent = ""
-
-    } else {
-      errorQuantity.textContent = "veuillez insérer un chiffre"
-      formError = formError + 1
-
-    }
-  } else {
-    errorQuantity.textContent = "Veuillez insérer une qty"
-    formError = formError + 1
-
-  }
-
-
-  //Validation villes
-  let checkboxes = document.querySelectorAll('input[name="location"]:checked');
-  let values = [];
-  checkboxes.forEach((checkbox) => {
-    values.push(checkbox.value);
-  });
-  if (values.length > 0) {
-    console.log("ville valide")
-    errorLocation.textContent = ""
-
-  } else {
-
-    errorLocation.textContent = "Veuillez choisir une ville"
-    formError = formError + 1
-
-  }
-
-  //Validation conditions d'utilisation
-  if (checkbox.checked) {
-    errorCheckBox.textContent = ""
-
-  } else {
-    errorCheckBox.textContent = "Veuillez accepter les conditions d'utilisation"
-    formError = formError + 1
-
-  }
+  return true;
 }
+
+
+//Validation nombre tournoi
+function quantityValidation(event) {
+  errorQuantity.textContent = ""
+  const regexQuantity = /^(0|[1-9][0-9]*)$/;
+  if (!regexQuantity.test(quantity.value)) {
+    errorQuantity.textContent = "veuillez insérer un chiffre"
+    return false;
+  }
+  return true;
+}
+
+
+//Validation villes
+function cityValidation() {
+  errorLocation.textContent = "";
+
+  if ([...document.querySelectorAll('input[name="location"]:checked')].length < 1) {
+    errorLocation.textContent = "Veuillez choisir une ville"
+    return false;
+  }
+
+  console.log("ville valide");
+  return true;
+}
+
+//Validation conditions d'utilisation
+function CGUValidation() {
+  errorCheckBox.textContent = "";
+
+  if (!checkbox.checked) {
+    errorCheckBox.textContent = "Veuillez accepter les conditions d'utilisation";
+    return false;
+  }
+
+  console.log("CGU validé");
+  return true;
+}
+
+firstName.onchange = () => { firstNameValidation() }
+lastName.onchange = () => { lastNameValidation() }
+email.onchange = () => { emailValidation() }
+birthDate.onchange = () => { birthDateValidation() }
+quantity.onchange = () => { quantityValidation() }
+checkLocation.forEach(i => i.oninput = () => cityValidation());
+checkbox.onchange = () => { CGUValidation() }
 
 
 //Submit form
 let submit = document.querySelector(".btn-submit");
 submit.addEventListener('click', (event) => {
   event.preventDefault();
-  formValidation(this)
-  console.log(formError)
-  if (formError == 0) {
-    submit.submit
-    let modal = document.getElementById("reserve")
-    modal.style.display = "none"
-    modalValidation.style.display = "block"
-
-  } else {
-    alert("remplir formulaire")
-    formError = 0
+  if (formValidation(this)) {
+    console.log("C'est bon !")
+    // submit.submit
+    const modal = document.getElementById("reserve")
+    CloseModal(modal);
+    OpenModal(modalValidation);
   }
-})
-
-
-//Close modal
-let modalValidation = document.getElementById("validation")
-modalValidation.addEventListener("click", (event) => {
-
-  closeModal()
-
 })
